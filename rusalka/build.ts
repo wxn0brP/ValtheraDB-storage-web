5ca96d4fab410b505283f00b26b1a6556d0980fb
wxn0brP <toja.filiper@gmail.com>
2026-02-28 18:04:02 +0100

    chore: update rusalka config

--- rusalka/build.ts
+++ rusalka/build.ts
@@ -1,6 +1,1 @@
-import { readFileSync, writeFileSync } from "fs";
 export const publishToNpm = true;
-export function postBuildFn() {
-    const version = JSON.parse(readFileSync("./package.json", "utf-8")).version;
-    writeFileSync("./dist/version.js", `export const version = "${version}";`);
-}
